3225137cd9ab1f8bf76a5459e427447e4c86393d
陈锦庭 <495177767@qq.com>
2018-01-31 19:04:51 +0800

    User can create topic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Topic;
+use App\Observers\TopicObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Carbon\Carbon::setLocale('zh');
+        Topic::observe(TopicObserver::class);
     }
 
     /**
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -38,7 +38,13 @@
                     <a href="{{ route('register') }}">注册</a>
                 </li>
                 @else
+                <li>
+                        <a href="{{ route('topics.create') }}">
+                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                        </a>
+                    </li>
                 <li class="dropdown">
+                
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                         <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
                         <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
@@ -49,6 +55,7 @@
                     </a>
 
                         <ul class="dropdown-menu" role="menu">
+                        
                             <li>
                                 <a href="{{ route('users.show', Auth::id()) }}">
                                     <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
