99fbfee616d79ce94493a258b964362979f78cf4
陈锦庭 <495177767@qq.com>
2018-01-31 20:00:00 +0800

    User can upload topic image

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Requests;
+use App\Handlers\ImageUploadHandler;
 
 class TopicRequest extends Request
 {
@@ -40,5 +41,27 @@ class TopicRequest extends Request
             'title.min' => '标题必须至少两个字符',
             'body.min' => '文章内容必须至少三个字符',
         ];
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败!',
+            'file_path' => ''
+        ];
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = "上传成功!";
+                $data['success']   = true;
+            }
+        }
+        return $data;
     }
 }
